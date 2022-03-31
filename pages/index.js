4bc800790a0595adddfde72421492c6eaf61785d
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-03-31 18:38:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="[Click here to enter Worcester Ski Supply](https://csiltler.wixsite.com/worcesterskisupply)" />
+        <Header title=[Click here to enter Worcester Ski Supply](https://csiltler.wixsite.com/worcesterskisupply) />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
