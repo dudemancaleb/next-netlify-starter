f9542634e7621dac99799ffe3572d22d7bddd8cf
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-03-31 19:50:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+    html { 
+  background: url(jump.png) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
     <div className="container">
       <Head>
         <title>Worcester Ski Supply</title>
