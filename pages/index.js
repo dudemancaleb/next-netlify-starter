ab0a65133ed4c936586550a24c9a316f5fc2968a
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-03-31 22:53:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,12 @@ export default function Home() {
       </Head>
 
       <main>
-        document.body.style.backgroundImage = "url('jump.png')";
+        
         <Header title="Welcome to Worcester Ski Supply!" />
         <p className="description">
           <a href="https://csiltler.wixsite.com/worcesterskisupply"> Click Here to Enter </a>
         </p>
+          document.body.style.backgroundImage = "url('jump.png')";
       </main>
 
       <Footer />
