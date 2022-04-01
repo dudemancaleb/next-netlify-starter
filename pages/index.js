4419e8b0724f92d5f8b8c35df26356f6c466a4e2
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-04-01 14:45:24 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,18 +29,4 @@ export default function Home() {
     </div>
   )
 }
-<html>
-<body>
 
-<h1>Hello World!</h1>
-
-<button type="button" onclick="myFunction()">Set background color</button>
-
-<script>
-function myFunction() {
-  document.body.style.backgroundColor = "red";
-}
-</script>
-
-</body>
-</html>
