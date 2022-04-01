3098d1aadacc4c130639129742e4f62d57f1ea61
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-03-31 22:05:05 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+document.body.style.backgroundImage = "url('jump.png')";
+
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
- 
+    
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
