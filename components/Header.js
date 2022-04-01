be667360f7d2b368ff78be622b87595e8da86053
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-04-01 15:00:15 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,4 @@
 
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
-   <html>
-   <body>
-
-   <h1>Hello World!</h1>
-
-   <button type="button" onclick="myFunction()">Set background color</button>
-
-   <script>
-   function myFunction() {
-    document.body.style.backgroundColor = "red";
-  }
-  </script>
-
-  </body>
-  </html>
-
 }
