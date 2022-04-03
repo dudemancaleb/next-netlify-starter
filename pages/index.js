f8035840d44c2bb4d9f237507b5f391b5636a4cc
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-04-03 19:25:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,16 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html>
+<body>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        
-        <title>Worcester Ski Supply</title>
-        <link rel="icon" href="/favicon.ico" />
-       
-      </Head>
+<h1>Hello World!</h1>
 
-      <main>
-        
+<button type="button" onclick="myFunction()">Set background color</button>
 
-
-        <Header title="Welcome to Worcester Ski Supply!" />
-        <p className="description">
-          <a href="https://csiltler.wixsite.com/worcesterskisupply"> Click Here to Enter </a>
-        </p>
-       
-      </main>
-   
-      <Footer />
-    </div>
-  )
+<script>
+function myFunction() {
+  document.body.style.backgroundColor = "red";
 }
+</script>
+
+</body>
+</html>
