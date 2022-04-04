f314c5c0cea77e87b0316b1344f0c8eda9713d27
dudemancaleb <83992124+dudemancaleb@users.noreply.github.com>
2022-04-03 23:59:56 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,9 @@
 
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
+    <style>
+       background-image:
+         linear-gradient(to bottom, rgba(255,255,0,0.5), rgba(0,0,255,0.5)),
+         url('jump.png');
+    </style>
 }
